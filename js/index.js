923edc089a35cf6c441054552e685a60b4fd3c54
Mrxiaov <51041769+Mrxiaov@users.noreply.github.com>
2021-08-23 15:24:03 +0800

    左右饼图

--- js/index.js
+++ js/index.js
@@ -448,3 +448,110 @@
 	})
 })();
 
+// 饼图左
+(function () {
+	let myChart = echarts.init(document.querySelector(".pie .chart"))
+	const option = {
+		tooltip: {
+			trigger: 'item'
+		},
+		color: ["#065aab", "#066eab", "#0682ab", "#0696ab", "#06a0ab",],
+		legend: {
+			// 距离底部为0%
+			bottom: "0%",
+			// 小图标的宽度和高度
+			itemWidth: 10,
+			itemHeight: 10,
+			data: ["0岁以下", "20-29岁", "30-39岁", "40-49岁", "50岁以上"],
+			// 修改图例组件的文字为 12px
+			textStyle: {
+				color: "rgba(255,255,255,.5)",
+				fontSize: "12"
+			}
+		},
+		series: [
+			{
+				name: "年龄分布",
+				type: "pie",
+				// 设置饼形图在容器中的位置
+				center: ["50%", "50%"],
+				//  修改内圆半径和外圆半径为  百分比是相对于容器宽度来说的
+				radius: ["40%", "60%"],
+				// 不显示标签文字
+				label: { show: true },
+				// 不显示连接线
+				labelLine: { show: true },
+				data: [
+					{ value: 1, name: "0岁以下" },
+					{ value: 4, name: "20-29岁" },
+					{ value: 2, name: "30-39岁" },
+					{ value: 2, name: "40-49岁" },
+					{ value: 1, name: "50岁以上" }
+				],
+			}
+		]
+	}
+	myChart.setOption(option)
+	window.addEventListener("resize", function () {
+		myChart.resize();
+	})
+})();
+
+// 饼图右
+(function () {
+	let myChart = echarts.init(document.querySelector(".pie-right .chart"))
+	const option = {
+		color: ['#006cff', '#60cda0', '#ed8884', '#ff9f7f', '#0096ff', '#9fe6b8', '#32c5e9', '#1d9dff'],
+		tooltip: {
+			trigger: 'item',
+			formatter: '{a} <br/>{b} : {c} ({d}%)'
+		},
+		legend: {
+			bottom: "0%",
+			itemWidth: 8,
+			itemHeight: 8,
+			textStyle: {
+				color: "rgba(255,255,255,.5)",
+				fontSize: "12"
+
+			}
+		},
+		series: [
+			{
+				name: '面积模式',
+				type: 'pie',
+				radius: ['10%', '60%'],
+				center: ['45%', '50%'],
+				roseType: 'radius',
+				itemStyle: {
+					borderRadius: 5
+				},
+				label: {
+					fontSize: 10
+				},
+				labelLine: {
+					// 连接扇形图线长
+					length: 6,
+					// 连接文字线长
+					length2: 8
+				},
+				data: [
+					{ value: 20, name: '云南' },
+					{ value: 26, name: '北京' },
+					{ value: 24, name: '山东' },
+					{ value: 25, name: '河北' },
+					{ value: 20, name: '江苏' },
+					{ value: 25, name: '浙江' },
+					{ value: 30, name: '四川' },
+					{ value: 42, name: '湖北' }
+				]
+			}
+		]
+	}
+	myChart.setOption(option)
+	window.addEventListener("resize", function () {
+		myChart.resize();
+	})
+})()
+
+//航线图
